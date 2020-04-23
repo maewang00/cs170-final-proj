2d89523b8228625f917528dc44204bed66950a31
Zoe <yunqiz@berkeley.edu>
2020-04-23 15:09:23 -0700

    jahnvi changes

--- solver.py
+++ solver.py
@@ -1,9 +1,9 @@
 import networkx as nx
 from parse import read_input_file, write_output_file
-from utils import is_valid_network, average_pairwise_distance
+from utils import is_valid_network, average_pairwise_distance_fast
 import sys
 from heapq import heappush, heappop
-
+import copy
 def solve(G):
     """
     Args:
@@ -16,14 +16,35 @@ def solve(G):
     for e in G.edges:
         heappush(pq, (-G.edges[e]['weight'], e))
     
-    T = G
+    T = copy.deepcopy(G)
+    print(T == G)
+    print(type(T))
+    costpq = []
+    
     while pq:
+        node = heappop(pq)
+        e = node[1]
+        print(e)
+        w = node[0]
+        T.remove_edge(e[0], e[1])
+        if T.degree(e[1]) == 0:
+            T.remove_node(e[1])
+        if T.degree(e[0]) == 0:
+            T.remove_node(e[0])
+        # print("CONNECT")
+        # print()
+        if nx.is_connected(T) and nx.is_dominating_set(G, T):
+            print("SDFSDF")
+            if nx.is_tree(T):
+                heappush(costpq, (average_pairwise_distance_fast(T), T))
+                cost = average_pairwise_distance_fast(T)
+        else:
+            T.add_edge(e[0], e[1], weight=w)
+    # return 0
+    return heappop(costpq)[0]
 
-        e = heappop(pq)[1]
-        
-        w = G.edges[e]['weight']
-        T = T.remove_edge(e)
-        
+gr = read_input_file('inputs/small-41.in')
+print(solve(gr))
         
     # print(list(G.edges))
     # print(nx.is_dominating_set(G, G.nodes))
@@ -35,11 +56,11 @@ def solve(G):
 
 # Usage: python3 solver.py test.in
 
-if __name__ == '__main__':
-    # assert len(sys.argv) == 2
-    # path = sys.argv[1]
-    G = read_input_file("inputs/small-111.in")
-    solve(G)
+# if __name__ == '__main__':
+#     # assert len(sys.argv) == 2
+#     # path = sys.argv[1]
+#     G = read_input_file("inputs/small-111.in")
+#     solve(G)
     # assert is_valid_network(G, T)
     # print("Average  pairwise distance: {}".format(average_pairwise_distance(T)))
     # write_output_file(T, 'out/test.out')

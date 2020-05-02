57e6f80026cbd96fa93e9ed155237d21f1aa7f1d
Mae Wang <maewang@berkeley.edu>
2020-05-02 14:57:04 -0700

    graphs can't compare-big sad

--- README.md
+++ README.md
@@ -11,3 +11,19 @@ When writing inputs/outputs:
 - Make sure you use the functions `write_input_file` and `write_output_file` provided
 - Run the functions `read_input_file` and `read_output_file` to validate your files before submitting!
   - These are the functions run by the autograder to validate submissions
+  
+--------------------------------------------------------------------------------------------------
+PARSING INSTRUCTIONS:
+
+1. Simply run the file directly: "python solver.py" or "python3 solver.py". This will directly call the function makeAllOutputFiles(), which will solve and build all output files into the "outputs" folder, given that there is a folder called "inputs" in the same path directory as solver.py. Make sure utils.py and parse.py are also in the same path as well.
+
+--------------------------------------------------------------------------------------------------
+Files in the same directory level for correct building:
+
+1. solver.py
+2. utils.py
+3. parse.py
+4. inputs folder with .in files
+5. outputs folder (after running solver.py, .out files will be here)
+
+
--- solver.py
+++ solver.py
@@ -4,6 +4,7 @@ from utils import is_valid_network, average_pairwise_distance_fast
 import sys
 from heapq import heappush, heappop
 import copy
+import random
 import os
 def solve(G):
     """
@@ -13,19 +14,24 @@ def solve(G):
     Returns:
         T: networkx.Graph
     """
+    #For input graphs of 1 vertex (with possible trick self-loops)
+    if G.number_of_nodes() == 1:
+        T = nx.Graph()
+        T.add_node(0)
+        return T
 
     #For input graphs of 2 vertices and an edge between them
     if G.number_of_nodes() == 2:
-        print("HI")
+        #print("HI")
         e = G.edges[list(G.edges)[0]]['weight'] #list(G.edges)[0]
         G.edges[list(G.edges)[0]]['weight'] = 0.000
         return G
 
     #For input graphs where 1 vertex spans all other vertices
     spanner_nodes = [node for node in G if is_spanning(G, node)]
-    print(spanner_nodes)
+    #print(spanner_nodes)
     if (len(spanner_nodes) > 0):
-        print("YO")
+        #print("YO")
         T = nx.Graph()
         if (spanner_nodes[0] == 1):
             T.add_weighted_edges_from([(spanner_nodes[0],2,0)])
@@ -33,14 +39,13 @@ def solve(G):
             T.add_weighted_edges_from([(spanner_nodes[0],1,0)])
         return T
 
-    pq = []
+    pq = [] #min heap for Kruskals edge popping
     for e in G.edges:
         heappush(pq, (-G.edges[e]['weight'], e))
-    
     T = copy.deepcopy(G)
-    # print(T == G)
-    # print(type(T))
-    costpq = []
+    costpq = [] #min heap with minimal pairwise distance with its tree
+    MST = nx.minimum_spanning_tree(G)
+    heappush(costpq, (average_pairwise_distance_fast(MST), MST))
     
     while pq:
         if (T.number_of_nodes() == 2):
@@ -54,16 +59,40 @@ def solve(G):
             T.remove_node(e[1])
         if T.degree(e[0]) == 0:
             T.remove_node(e[0])
-        # print("CONNECT")
-        # print()
         if nx.is_connected(T) and nx.is_dominating_set(G, T):
-            # print("SDFSDF")
             if nx.is_tree(T):
                 heappush(costpq, (average_pairwise_distance_fast(T), T))
                 # cost = average_pairwise_distance_fast(T)
         else:
             T.add_edge(e[0], e[1], weight=w)
-    # return 0
+
+    # #randomize version for 10 iterations
+    # iterations = 10
+    # while (iterations):
+    #     #random.shuffle(G.edges)
+    #     for e in G.edges:
+    #         heappush(pq, (-G.edges[e]['weight'], e))
+    #     _T = copy.deepcopy(G)
+
+    #     while pq:
+    #         if (_T.number_of_nodes() == 2):
+    #             break
+    #         edge = heappop(pq)
+    #         e = edge[1]
+    #         w = edge[0] * -1
+    #         _T.remove_edge(e[0], e[1])
+    #         if _T.degree(e[1]) == 0:
+    #             _T.remove_node(e[1])
+    #         if _T.degree(e[0]) == 0:
+    #             _T.remove_node(e[0])
+    #         if nx.is_connected(_T) and nx.is_dominating_set(G, _T):
+    #             if nx.is_tree(_T):
+    #                 heappush(costpq, (average_pairwise_distance_fast(_T), _T))
+    #                 # cost = average_pairwise_distance_fast(T)
+    #         else:
+    #             _T.add_edge(e[0], e[1], weight=w)
+    #     iterations -= 1
+
     return heappop(costpq)[1]
     
 
@@ -73,7 +102,7 @@ def is_spanning(G, node):
 def makeAllOutputFiles():
     for file in os.listdir("inputs"):
         if file.endswith(".in"):
-            #print(os.path.join("inputs", file)) #input file
+            print(os.path.join("inputs", file)) #input file
             input_path = os.path.join("inputs", file)
             G = read_input_file(input_path)
             T = solve(G)
@@ -81,29 +110,30 @@ def makeAllOutputFiles():
             print("Average pairwise distance: {}".format(average_pairwise_distance_fast(T)))
             outname = os.path.splitext(file)[0]+'.out'
             output_path = os.path.join("outputs", outname)
-            #print(output_path + "\n")
+            print(output_path + "\n")
             write_output_file(T, output_path)
 
 
 def validateAllFiles():
     for file in os.listdir("outputs"):
         output_path = os.path.join("outputs", file)
-        validate_file(output_path)
-        #print(output_path + " validated.")
+        if (validate_file(output_path) == False):
+            print(output_path + " INVALIDATED.")
+        
 
 
-#makeAllOutputFiles()
-#validateAllFiles()
-
-# gr = read_input_file('inputs/small-4.in')
+makeAllOutputFiles()
+validateAllFiles()
+# gr = read_input_file('inputs/small-254.in')
 # s = solve(gr)
 # print(average_pairwise_distance_fast(s))
+# write_output_file(s, 'outputs/small-254.out')
 # print(is_valid_network(gr,s))
+
         
     # print(list(G.edges))
     # print(nx.is_dominating_set(G, G.nodes))
     # print(average_pairwise_distance(G))
-
 
 
 # Here's an example of how to run your solver.
